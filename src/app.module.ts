2804eea6622a20ec66f3b2c8507a6f9fcce1e898
abijith.v <abijith.venugopalan@dreamguystech.com>
2022-06-01 11:30:09 +0530

    global added'

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { UsersModule } from "./users/users.module";
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: "./.env" }),
+    ConfigModule.forRoot({ envFilePath: "./.env", isGlobal: true }),
     UsersModule,
     PassportModule,
     MongooseModule.forRoot(
