a77fc976b9414ee6f0cd37e3e1da3370e22b04b5
abijith.v <abijith.venugopalan@dreamguystech.com>
2022-06-01 11:38:24 +0530

    200 added'

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Hello World!";
+    throw new HttpException({ status: 200, message: "Nest connected!" }, 200);
   }
 }
